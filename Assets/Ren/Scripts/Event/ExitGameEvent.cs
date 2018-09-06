c9a3343ad877360509963324da5daeb380716082
Renelie Salazar <rgsalazar@voyagerinnovation.com>
2018-09-06 21:21:55 +0800

    - Coded ExitGameEvent - Readied the ExitGamePanel - Created TimeLine for ExitGamePanel OPEN and CLOSE

---
+++ Assets/Ren/Scripts/Event/ExitGameEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ExitGameEvent : InGameEvent {
+
+	protected override bool FireNow() {
+		LogUtil.PrintInfo(gameObject, GetType(), "Exiting game. Thank you for playing... :)");
+		Application.Quit();
+		return true;
+	}
+
+	public override void CancelNow() {
+		LogUtil.PrintInfo(gameObject, GetType(), "Sorry, you cannot cancel an ExitGame event's operation.");	
+	}
+
+}
