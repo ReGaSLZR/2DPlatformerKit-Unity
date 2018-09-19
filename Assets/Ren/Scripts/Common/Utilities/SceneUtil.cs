e46d2d271939f4b4f2f0bfc50909a60e2cf386b7
Renelie Salazar <rgsalazar@voyagerinnovation.com>
2018-09-19 18:44:42 +0800

    - Added SceneUnloader for Loading.unity scene - Fixed the MainMenu content panels size - Fixed the MainMenu panels TimeLine animations

--- Assets/Ren/Scripts/Common/Utilities/SceneUtil.cs
+++ Assets/Ren/Scripts/Common/Utilities/SceneUtil.cs
@@ -4,8 +4,8 @@ using UnityEngine.SceneManagement;
 public class SceneUtil {
 
 //	private static int SPLASH = 0;
-	private static int MAIN_MENU = 1;
-	private static int LOADING = 2;
+	public static int MAIN_MENU = 1;
+	public static int LOADING = 2;
 
 	//NOTE: 2 == default highest level cleared in-game | Zero-based | refer to SceneIndexes
 	public static string NON_WORLD_LEVEL_SCENES = "2";
@@ -18,6 +18,10 @@ public class SceneUtil {
 	public static void LoadScene(int index) {
 		SceneManager.LoadSceneAsync(LOADING, LoadSceneMode.Additive);
 		SceneManager.LoadSceneAsync(index);
+	}
+
+	public static void UnloadScene(int index) {
+		SceneManager.UnloadSceneAsync(index);
 	}
 
 	public static int GetSceneIndex_Current() {
---
+++ Assets/Ren/Scripts/States/SceneUnloader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneUnloader : MonoBehaviour
+{
+
+	[SerializeField] private int sceneIndexToUnload = SceneUtil.LOADING;
+	[SerializeField] private float unloadDelay = 7f;
+
+	private void Start()
+	{
+		StartCoroutine(CorUnloadScene());
+	}
+
+	private IEnumerator CorUnloadScene() {
+		yield return new WaitForSeconds(unloadDelay);
+
+		LogUtil.PrintInfo(gameObject, GetType(), "Unloading Scene with index " + sceneIndexToUnload);
+		SceneUtil.UnloadScene(sceneIndexToUnload);
+	}
+
+	private void OnDisable() {
+		StopAllCoroutines();
+	}
+
+}
+
